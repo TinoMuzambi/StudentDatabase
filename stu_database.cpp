a7e5756dc266919f51377ceb236de7acbaaf8ff2
Tino <tinomuzambi@gmail.com>
2020-02-16 20:08:00 +0200

    Initial commit.

---
+++ stu_database.cpp
@@ -0,0 +1,10 @@
+//
+// Created by Asus Pro on 2020/02/16.
+//
+
+#include "stu_database.h"
+#include "cstdio"
+
+int main() {
+    puts("Wassup");
+}
---
+++ stu_database.h
@@ -0,0 +1,14 @@
+//
+// Created by Asus Pro on 2020/02/16.
+//
+
+#ifndef ASSIGNMENT_1_STU_DATABASE_H
+#define ASSIGNMENT_1_STU_DATABASE_H
+
+
+class stu_database {
+
+};
+
+
+#endif //ASSIGNMENT_1_STU_DATABASE_H
